6dfa7cb0130b83471a3551af6e8d327445a7df50
nilzoft <65558571+nilzoft@users.noreply.github.com>
2020-05-20 02:07:26 -0500

    Create #2_Even_Fibonacci_numbers.c

---
+++ #2_Even_Fibonacci_numbers.c
@@ -0,0 +1,32 @@
+/*
+ * EJERCICIO RESUELTO POR: Nilzon Vela C.
+ * FECHA: 19/05/2020
+ * GITHUB: https://github.com/nilzoft/
+ */
+
+#include <stdio.h>
+
+int sum_fibonacci();
+
+int main() {
+    printf("%u", sum_fibonacci());
+    return 0;
+}
+
+int sum_fibonacci() {
+    unsigned int sum = 0,
+                 A, B, C;
+    A = 1;
+    B = 2;
+    C = A + B;
+    sum = 2;
+    
+    while (C <= 4000000) {
+        if (C % 2 == 0)
+            sum += C;
+        A = B;
+        B = C;
+        C = A + B;
+    }
+    return sum;
+}
