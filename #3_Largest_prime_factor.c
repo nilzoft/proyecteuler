051d83c60b1633fe05cd73add6473a9851ed3928
nilzoft <65558571+nilzoft@users.noreply.github.com>
2020-05-21 13:27:43 -0500

    Create #3_Largest_prime_factor.c

---
+++ #3_Largest_prime_factor.c
@@ -0,0 +1,32 @@
+/*
+ * EJERCICIO RESUELTO POR: Nilzon Vela C.
+ * FECHA: 20/05/2020
+ * GITHUB: https://github.com/nilzoft/
+ * LINK PROBLEM: https://projecteuler.net/problem=3
+ */
+ 
+#include <stdio.h>
+
+typedef unsigned long int big;
+
+big factorPrimo(big);
+
+int main() {
+    big num = 600851475143;
+    printf("El factor primo mas grade de = %lu es = %lu\n", num, factorPrimo(num));
+    return 0;
+}
+
+big factorPrimo(big num) {
+    big alt = 2,
+	factor = alt;    
+    while (alt <= num) {
+    	if (num % alt == 0) {
+     	     num /= alt;
+	     factor = alt;
+	     printf("%lu\n", factor);
+	}
+	alt++;
+    }
+    return factor;
+}
